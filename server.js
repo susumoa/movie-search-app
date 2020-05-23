2d26a056b07de60296c59db3caea8fd49097e7cc
susumoa <tobiasbori@gmail.com>
2020-05-23 13:47:43 +0100

    add process.env.port

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const apiKey = process.env.API_KEY;
 const url = `http://www.omdbapi.com/?apikey=${apiKey}&`;
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
